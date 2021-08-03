fe1513d6efd4f647f74b3c3e6330235718a8de5d
mlinarik <40384547+mlinarik@users.noreply.github.com>
2021-08-02 21:09:03 -0400

    Update ark.sh

--- ark.sh
+++ ark.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+FILEV=/home/steam/steamcmd/ark/ShooterGame/Binaries/Linux/start_server.sh
+if [ -f "$FILEV" ]; then
+    cp /home/steam/steamcmd/ark/ShooterGame/Binaries/Linux/start_server.sh /tmp/start_server.sh
+    echo "LOG: Copied "
+fi
+
+echo "LOG: Copy Done"
+
 cd /home/steam/steamcmd
 
 echo "LOG: Start App_Update "
@@ -8,8 +16,17 @@ echo "LOG: Start App_Update "
 
 echo "LOG: App_Update Done"
 
+
+FILET=/tmp/start_server.sh
+if [ -f "$FILET" ]; then
+    mv /tmp/start_server.sh /home/steam/steamcmd/ark/ShooterGame/Binaries/Linux/start_server.sh
+    echo "LOG: Moved " $FILET
+fi
+
+echo "LOG: Move Done"
+
 echo "Start Server"
 
-cd .steam/steamcmd/ark/ShooterGame/Binaries/Linux/
+cd /home/steam/steamcmd/ark/ShooterGame/Binaries/Linux/
 
-./ark_start_server.sh
+./start_server.sh
