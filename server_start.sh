5427328f4b27400af14165f3f52762fdde47c921
mlinarik <40384547+mlinarik@users.noreply.github.com>
2022-06-18 13:46:41 -0400

    Create server_start.sh

---
+++ server_start.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+./steamcmd.sh +force_install_dir /ark +login anonymous +app_update 376030 +quit
+cd /ark/ShooterGame/Binaries/Linux
+./ShooterGameServer $MAP?listen?SessionName=$SERVER_NAME?ServerPassword=$SERVER_PASSWORD?ServerAdminPassword=$ADMIN_PASSWORD
+-server -log
